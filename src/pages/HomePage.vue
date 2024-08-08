ec043be4d3f91b9144df75cdb29e11d6f234176d
Yuri Kuichik <kuichikmis4enko@gmail.com>
2024-08-08 13:25:37 +0300

    first commit

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,21 @@
+<script>
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+    data() {
+        return {
+            message: 'Hello from Home page'
+        } 
+    },
+
+    components: {
+        HelloWorld
+    }
+}
+</script>
+
+<template>
+  <main>
+    <HelloWorld :msg="message" />
+  </main>
+</template>
